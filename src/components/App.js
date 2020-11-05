fdf8d762c09ccd2c5872aa90e5df457c2259be3e
Daisugi <volble124@gmail.com>
2020-11-06 00:17:49 +0900

    add sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
 import Login from './Login';
+import Dashboard from './Dashboard';
+import { ContactsProvider } from '../contexts/ContactsProvider';
+import { ConversationsProvider } from '../contexts/ConversationsProvider';
 
 function App() {
-  const [id, setId] = useState();
+  const [id, setId] = useLocalStorage('id');
+
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider>
+        <Dashboard id={id}/>
+      </ConversationsProvider>
+    </ContactsProvider>
+  )
 
   return (
     <>
-      <div>{id}</div>
-      <Login onIdSubmit={setId}/>
+      {id ? dashboard : <Login onIdSubmit={setId}/> }
     </>
   )
 }
