8d73093b52d0075a2deceff6d21b168a629446ff
Daisugi <volble124@gmail.com>
2020-11-05 16:06:59 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Login from './Login';
+
+function App() {
+  const [id, setId] = useState();
+
+  return (
+    <>
+      <div>{id}</div>
+      <Login onIdSubmit={setId}/>
+    </>
+  )
+}
+
+export default App;
